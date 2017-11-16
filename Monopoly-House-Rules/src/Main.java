9640186e59f75c25be551a9905a914ca5093efc4
fresh <fresh@Fresh-Ubuntu>
2017-11-16 19:46:31 -0400

    Changed the Sysout message.

--- Monopoly-House-Rules/src/Main.java
+++ Monopoly-House-Rules/src/Main.java
@@ -1,6 +1,6 @@
 //Main program to compile for playing
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("This'll eventually be a Monopoly game!");
 	}
 }
