07cce82518d7275516f197687ae481c8539b6f20
fresh <fresh@Fresh-Ubuntu>
2017-11-20 16:05:49 -0400

    Added a method to check whether the dice roll was a double or not.

--- Monopoly-House-Rules/src/Dicer.java
+++ Monopoly-House-Rules/src/Dicer.java
@@ -4,21 +4,31 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Dicer {
 
    public int firstDice(){
-        int dice;
-        Random generator = new Random();
-        dice = generator.nextInt(6) + 1;
-        return dice;
-   }
-   
-   public int secondDice(){
-       int dice;
-       Random generator = new Random();
-       dice = generator.nextInt(6) + 1;
+	   int dice;
+	   Random generator = new Random();
+	   dice = generator.nextInt(6) + 1;
        return dice;
   }
-   public int bothDice(){
-	   int dices = ThreadLocalRandom.current().nextInt(2, 12 + 1);
-	   return dices;
-   }
+   
+  public int secondDice(){
+	  int dice;
+      Random generator = new Random();
+      dice = generator.nextInt(6) + 1;
+      return dice;
+  }
+  
+  //Method to roll both dices at once in the case of landing in a Utility
+  public int bothDice(){
+	  int dices = ThreadLocalRandom.current().nextInt(2, 12 + 1); //the first number in the .nextInt(x, y+1) is the min while the second is the upper limit (you have to +1 it to have the desired limit)
+	  return dices;
+  }
+   
+  
+  //Method to check whether the dice roll is or isn't a double
+  public boolean doubleDices(int first, int second) {
+	  if (first == second) return true;
+	  else return false;
+  }
+   
 
 }
